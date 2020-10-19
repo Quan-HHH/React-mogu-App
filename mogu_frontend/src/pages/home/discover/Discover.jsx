2f6903efd2edebc44a12f446a5125a01487dd734
Quan-HHH <1411514716@qq.com>
2020-10-19 23:43:46 +0800

    swiper

--- mogu_frontend/src/pages/home/discover/Discover.jsx
+++ mogu_frontend/src/pages/home/discover/Discover.jsx
@@ -1,38 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './discover.css'
-import Swiper from 'swiper'
-import 'swiper/css/swiper.min.css'
+import Swiper from "swiper/dist/js/swiper"
+import "swiper/dist/css/swiper.css"
 
 
 function Discover() {
+    const [slideList] = useState([
+        {
+            id: 1,
+            src: "https://s5.mogucdn.com/mlcdn/c45406/201015_2h25ghj9j946736l66d5f846ikc67_1060x367.jpg",
+            linkUrl: '#'
+        },{
+            id: 2,
+            src: "https://s11.mogucdn.com/mlcdn/c45406/201018_01kc4569hk0j56ick9jd1g2e71d5e_1060x367.png",
+            linkUrl: '#'
+        },{
+            id: 3,
+            src: "https://s5.mogucdn.com/mlcdn/c45406/201016_82kee329358f2el561c90ie230d37_1060x367.jpg",
+            linkUrl: '#'
+        },{
+            id: 4,
+            src: "https://s5.mogucdn.com/mlcdn/c45406/201016_6g6d26iaf716gjhll6eef8d014cg2_1060x367.png",
+            linkUrl: '#'
+        }
+    ])
     useEffect(() => {
         new Swiper('.swiper-container', {
-            direction: 'vertical',//竖向轮播
-            loop: true,//无缝轮播
-            pagination: {//小圆点分页
+            
+            autoplay: 2000,
+            loop: true,
+            pagination : {
                 el: '.swiper-pagination',
             },
             // scrollbar: {//下划线分页
             //     el: '.swiper-scrollbar',
             // }
         })
-        return () => {
-            cleanup
-        }
     }, [])
     return (
         <>
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
+            <div className="swiper-container">
+                <div className="swiper-wrapper">
+                    {
+                        slideList.map((i, item) => {
+                            return <div className="swiper-slide" key={item.id}>
+                                <a href={item.linkUrl}>
+                                    <img src={item.src} alt="" width="100%" height="100%"/>
+                                </a>
+                            </div>
+                        })
+                    }
+                    
                 </div>
 
-                <div class="swiper-pagination"></div>
+                <div className="swiper-pagination"></div>
 
-                <div class="swiper-scrollbar"></div>
+                <div className="swiper-scrollbar"></div>
             </div>
 
         </>
