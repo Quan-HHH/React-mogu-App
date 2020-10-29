809ee8d7b76a1d1e8f450a2ec4e68f8a17fdfb41
Quan-HHH <1411514716@qq.com>
2020-10-29 19:50:43 +0800

    后端服务器

---
+++ mogu_backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const apiMocker = require('mocker-api');
+
+const app = express();
+// 使用代理
+// 跨域问题, => 已解决。 
+apiMocker(app, path.resolve('./mocker/mocker.js'))
+app.listen(8080);
